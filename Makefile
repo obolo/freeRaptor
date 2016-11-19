23deeb87258a8502173f8570ec369cb02c257209
Roberto <francescon.roberto@gmail.com>
2016-11-19 11:36:18 +0100

    Updated V0, V1 and J tables

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-test: test_raptor1.cpp LT.cpp Rcodec.cpp
+test: test_libR10.cpp libR10.cpp
 	g++ -std=c++11 -I/usr/local/include -L/usr/local/lib -g -o test_libR10 test_libR10.cpp libR10.cpp -I.
 
 clean:
--- test_libR10.cpp
+++ test_libR10.cpp
@@ -26,17 +26,32 @@ int main(int argc, char* argv[])
   uint64_t F = 2048; 
   uint8_t  W = 64;
   uint64_t P = 512;
-  R10Encoder enc(K, F, W, P);
+  
+  R10Encoder enc(K, F, W, P); // R10Encoder object example
+  
   std::cout <<"Here is K: " << (unsigned)enc.K << std::endl;
   enc.setS();
   std::cout <<"Here is S: " << (unsigned)enc.S << std::endl;
   enc.setH();
   std::cout <<"Here is H: " << (unsigned)enc.H << std::endl;
   std::cout << "Here is the encoding matrix:" << std::endl;
-  print_matrix(enc.A, enc.L + K, enc.L + K);
+  //  print_matrix(enc.A, enc.L + K, enc.L + K);
   uint16_t d = 4;
   uint16_t a = 6;
   uint16_t b = 7;
   uint16_t t[3] = {d, a, b};
+
+  std::string filename = "README.md";
+  std::ifstream f_stream(filename.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+  std::streampos size = f_stream.tellg();
+  char* buffer = new char[size];
+  f_stream.seekg(0, std::ios::beg);
+  f_stream.read(buffer, size);
+  f_stream.close();
+
+  std::cout << "Here the size of the size in bits: " << size*8 << std::endl;
+  std::string dd = "CAcca si beautiful";
+  std::cout << "Here the size: " << dd.size() << std::endl;
+  
 
 }
